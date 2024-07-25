1b12c54273ae3c8749e636dd3ef6d782f944dfbd
Jan Schill <janschill@proton.me>
2024-07-25 22:20:03 +0200

    Add ping check for Deploy

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/janschill/track-me/internal/server"
 )
 
 func main() {
+	ping := flag.Bool("ping", false, "Run a self-test and exit")
+	flag.Parse()
+
+	if *ping {
+		fmt.Println("Self-test passed")
+		os.Exit(0)
+	}
+
 	port := "8080"
 	log.Default().Println("Server starting on port " + port)
 	server := server.HttpServer(port)
